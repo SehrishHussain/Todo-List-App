c869c850d48682b8503c497dbcf6b9df03294438
SehrishHussain <sehrishaslam1@hotmail.com>
2023-09-17 15:23:22 +0500

    developed standalone executable

--- gui.py
+++ gui.py
@@ -1,18 +1,24 @@
 import functions
 import PySimpleGUI as sg
 import time
+import os
+
+if not os.path.exists("todos.txt"):
+    with open("todos.txt", "w") as file:
+        pass
 
 sg.theme("Black")
 
 clock = sg.Text('', key='clock')
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add")
+add_button = sg.Button(button_text="Add todo", size=(9,1), mouseover_colors="LightBlue2",
+                       tooltip="Add todo", key="Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45,10])
-edit_button = sg.Button("Edit")
-complete_button = sg.Button("Complete")
-exit_button = sg.Button("Exit")
+edit_button = sg.Button("Edit", tooltip="Edit todo", mouseover_colors="LightBlue2")
+complete_button = sg.Button(key="Complete", tooltip="Complete", button_text="Complete", mouseover_colors="LightBlue2")
+exit_button = sg.Button("Exit", tooltip="Exit", mouseover_colors="LightBlue2")
 
 layout = [[clock], [label], [input_box, add_button], [list_box, edit_button, complete_button], [exit_button]]
 
